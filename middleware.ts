25dda4fca28ab8b5b339d0b998ca07148dabb7ed
Rahul Krishna K A <rahulkrishna7602@gmail.com>
2024-07-18 06:15:02 +0530

    Minor commit

--- middleware.ts
+++ middleware.ts
@@ -5,4 +5,16 @@ export const config = {
   matcher: ["/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)"],
 };
 
-export default auth((req) => {});
+export default auth((req) => {
+  const url = req.nextUrl;
+  let hostname = req.headers
+    .get("host")!
+    .replace(".localhost:3000", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
+  const searchParams = req.nextUrl.searchParams.toString();
+  const path = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}` : ""}`;
+  if (!req.auth) {
+    if (path !== "/login") {
+      return NextResponse.redirect(new URL(`/login`, `http://${hostname}`));
+    }
+  }
+});
