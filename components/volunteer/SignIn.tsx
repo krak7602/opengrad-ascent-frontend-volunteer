796b7836cfd8bed6d13c88e68b0058f6cc6b48e6
Rahul Krishna K A <rahulkrishna7602@gmail.com>
2024-07-22 03:04:54 +0530

    Fix password reset

--- components/volunteer/SignIn.tsx
+++ components/volunteer/SignIn.tsx
@@ -21,16 +21,27 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 import { Input } from "@/components/ui/input";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { signInSchema } from "@/lib/zod";
-import { ForgotPasswordPopup } from "@/components/volunteer/ForgotPasswordPopup";
 import { useToast } from "@/components/ui/use-toast";
+import axios from "axios";
 
 export default function SignIn({ curRole }: { curRole: string }) {
   const router = useRouter();
   const { toast } = useToast();
+  const [open, setOpen] = React.useState(false);
+  const [emailForReset, setEmailForReset] = React.useState("");
+  const [emailSent, setEmailSent] = React.useState(false);
   const [showPassword, setShowPassword] = React.useState(false);
   const form = useForm<z.infer<typeof signInSchema>>({
     resolver: zodResolver(signInSchema),
@@ -47,6 +58,7 @@ export default function SignIn({ curRole }: { curRole: string }) {
       return { error: "Invalid fields!" };
     }
     const { email, password, role } = validatedFields.data;
+
     await signIn("credentials", {
       email: email,
       password: password,
@@ -62,93 +74,156 @@ export default function SignIn({ curRole }: { curRole: string }) {
     });
   }
 
+  async function resetFormSubmit() {
+    try {
+      if (emailForReset) {
+        const resp = await axios.post(
+          `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/profileset`,
+          {
+            destination: emailForReset,
+          },
+        );
+        if (resp.data.success) setEmailSent(true);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   async function togglePasswordVisiblity() {
     setShowPassword(!showPassword);
   }
 
   return (
-    <Form {...form}>
+    <div>
       <Card>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-          <CardHeader>
-            <CardTitle className="text-2xl font-bold">
-              Sign in to your account
-            </CardTitle>
-            <CardDescription>
-              Enter your email and password to access your volunteer account.
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className="space-y-4">
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem className="space-y-2">
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input placeholder="" {...field} required type="email" />
-                    </FormControl>
-                    <FormDescription></FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem className="relative space-y-2">
-                    <div className="flex items-center">
-                      <FormLabel>Password</FormLabel>
-                      <ForgotPasswordPopup />
-                    </div>
-                    <FormControl>
-                      <div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <CardHeader>
+              <CardTitle className="text-2xl font-bold">
+                Sign in to your account
+              </CardTitle>
+              <CardDescription>
+                Enter your email and password to access your volunteer account.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="space-y-4">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem className="space-y-2">
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
                         <Input
                           placeholder=""
                           {...field}
                           required
-                          type={showPassword ? "text" : "password"}
+                          type="email"
                         />
-                        <Button
-                          type="button"
-                          className="absolute top-9 right-1 h-7 w-7"
-                          size="icon"
-                          variant="ghost"
-                          onClick={togglePasswordVisiblity}
-                        >
-                          <EyeIcon
-                            className={
-                              showPassword ? "visible h-4 w-4" : "hidden"
-                            }
-                          />
-                          <EyeSlashIcon
-                            className={
-                              showPassword ? "hidden" : "visible h-4 w-4"
-                            }
-                          />
-                          <span className="sr-only">
-                            Toggle password visibility
-                          </span>
-                        </Button>
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem className="relative space-y-2">
+                      <div className="flex items-center">
+                        <FormLabel>Password</FormLabel>
+                        {/* <ForgotPasswordPopup /> */}
                       </div>
-                    </FormControl>
-                    <FormDescription></FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+                      <FormControl>
+                        <div>
+                          <Input
+                            placeholder=""
+                            {...field}
+                            required
+                            type={showPassword ? "text" : "password"}
+                          />
+                          <Button
+                            type="button"
+                            className="absolute top-9 right-1 h-4 w-7 "
+                            size="icon"
+                            variant="ghost"
+                            onClick={togglePasswordVisiblity}
+                          >
+                            <EyeIcon
+                              className={
+                                showPassword ? "visible h-4 w-4" : "hidden"
+                              }
+                            />
+                            <EyeSlashIcon
+                              className={
+                                showPassword ? "hidden" : "visible h-4 w-4"
+                              }
+                            />
+                            <span className="sr-only">
+                              Toggle password visibility
+                            </span>
+                          </Button>
+                        </div>
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button className="w-full" type="submit">
+                Sign in
+              </Button>
+            </CardFooter>
+          </form>
+        </Form>
+        <Drawer open={open} onOpenChange={setOpen}>
+          <DrawerTrigger className="ml-auto inline-block text-sm text-gray-500 underline hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 pl-6 pb-5">
+            Forgot your password?
+          </DrawerTrigger>
+          <DrawerContent className="pb-6">
+            <DrawerHeader className="text-left">
+              <DrawerTitle className="text-2xl font-bold px-1">
+                Reset your password
+              </DrawerTitle>
+            </DrawerHeader>
+            <DrawerDescription className="px-4 pb-3">
+              Provide your email to send instructions to reset the password
+            </DrawerDescription>
+            <div className="px-4">
+              {emailSent && (
+                <div>
+                  <MailAccountIcon className="mx-auto my-5" />
+                  <div className="px-4 text-center text-sm text-neutral-600">
+                    An email will be sent to your account with the password
+                    reset instructions if an valid profile exists.
+                  </div>
+                </div>
+              )}
+              {!emailSent && (
+                <form className="flex flex-col gap-4">
+                  <Input
+                    type="email"
+                    placeholder=""
+                    onChange={(e) => {
+                      setEmailForReset(e.target.value);
+                    }}
+                  />
+                  <Button type="button" onClick={resetFormSubmit}>
+                    Send Reset Email
+                  </Button>
+                </form>
+              )}
             </div>
-          </CardContent>
-          <CardFooter>
-            <Button className="w-full" type="submit">
-              Sign in
-            </Button>
-          </CardFooter>
-        </form>
+          </DrawerContent>
+        </Drawer>
       </Card>
-    </Form>
+    </div>
   );
 }
 
@@ -204,6 +279,42 @@ const EyeSlashIcon = (props: React.SVGProps<SVGSVGElement>) => (
       strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
+    />
+  </svg>
+);
+
+const MailAccountIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={60}
+    height={60}
+    color={"#22c55e"}
+    fill={"none"}
+    {...props}
+  >
+    <path
+      d="M2 5L8.91302 8.92462C11.4387 10.3585 12.5613 10.3585 15.087 8.92462L22 5"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M21.996 10.5024C21.9933 10.1357 21.9894 9.77017 21.9842 9.5265C21.9189 6.46005 21.8862 4.92682 20.7551 3.79105C19.6239 2.65528 18.0497 2.61571 14.9012 2.53658C12.9607 2.48781 11.0393 2.48781 9.09882 2.53657C5.95033 2.6157 4.37608 2.65526 3.24495 3.79103C2.11382 4.92681 2.08114 6.46003 2.01576 9.52648C1.99474 10.5125 1.99475 11.4926 2.01577 12.4786C2.08114 15.5451 2.11383 17.0783 3.24496 18.2141C4.37608 19.3498 5.95033 19.3894 9.09883 19.4685C9.7068 19.4838 10.4957 19.4943 11 19.5"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M15.586 18.6482C14.9572 19.0167 13.3086 19.7693 14.3127 20.711C14.8032 21.171 15.3495 21.5 16.0364 21.5H19.9556C20.6424 21.5 21.1887 21.171 21.6792 20.711C22.6834 19.7693 21.0347 19.0167 20.4059 18.6482C18.9314 17.7839 17.0605 17.7839 15.586 18.6482Z"
+      stroke="currentColor"
+      strokeWidth="1.5"
+    />
+    <path
+      d="M19.996 14C19.996 15.1046 19.1005 16 17.996 16C16.8914 16 15.996 15.1046 15.996 14C15.996 12.8954 16.8914 12 17.996 12C19.1005 12 19.996 12.8954 19.996 14Z"
+      stroke="currentColor"
+      strokeWidth="1.5"
     />
   </svg>
 );
