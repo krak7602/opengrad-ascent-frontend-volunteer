64f49b0f41e87e0f813296adec797171b2607f5d
Rahul Krishna K A <rahulkrishna7602@gmail.com>
2024-07-21 07:53:22 +0530

    Feedback error fix #1

--- components/volunteer/Feedback.tsx
+++ components/volunteer/Feedback.tsx
@@ -74,7 +74,7 @@ export default function Feedback({
         });
       });
     }
-  }, [data, setRespItems]);
+  }, [data]);
 
   const handleDescChange = (
     e: React.ChangeEvent<HTMLTextAreaElement>,
